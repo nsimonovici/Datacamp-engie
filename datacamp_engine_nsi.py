32dd64aed0faa864531e0e5683a8f2cd134e765b
noel simonovici <noel.simonovici@edf.fr>
2025-02-17 13:17:56 +0100

    initialize repo without data

---
+++ datacamp_engine_nsi.py
@@ -0,0 +1,93 @@
+from regression_model_comparison import RegressionModelComparison
+import numpy as np
+import pandas as pd
+
+dataX = pd.read_csv('engie_X.csv', header=0, sep=';', decimal='.')
+dataY = pd.read_csv('engie_Y.csv', header=0,  sep=';', decimal='.')
+data_raw = pd.merge(dataX, dataY, on='ID', how='inner')
+print("SHAPE = ", data_raw.shape)
+
+sample_size = 100
+X = data_raw.drop(columns=['ID', 'MAC_CODE']).iloc[:sample_size, :]
+Y = data_raw.TARGET.iloc[:sample_size]
+
+comparison = RegressionModelComparison(
+    X,
+    Y,
+    scorings=['mae', 'mse'],
+    test_size=0.1,
+    seed=3
+    )
+
+numerical_features = [
+    'Date_time', 'Pitch_angle', 'Pitch_angle_min',
+       'Pitch_angle_max', 'Pitch_angle_std', 'Hub_temperature',
+       'Hub_temperature_min', 'Hub_temperature_max', 'Hub_temperature_std',
+       'Generator_converter_speed', 'Generator_converter_speed_min',
+       'Generator_converter_speed_max', 'Generator_converter_speed_std',
+       'Generator_speed', 'Generator_speed_min', 'Generator_speed_max',
+       'Generator_speed_std', 'Generator_bearing_1_temperature',
+       'Generator_bearing_1_temperature_min',
+       'Generator_bearing_1_temperature_max',
+       'Generator_bearing_1_temperature_std',
+       'Generator_bearing_2_temperature',
+       'Generator_bearing_2_temperature_min',
+       'Generator_bearing_2_temperature_max',
+       'Generator_bearing_2_temperature_std', 'Generator_stator_temperature',
+       'Generator_stator_temperature_min', 'Generator_stator_temperature_max',
+       'Generator_stator_temperature_std', 'Gearbox_bearing_1_temperature',
+       'Gearbox_bearing_1_temperature_min',
+       'Gearbox_bearing_1_temperature_max',
+       'Gearbox_bearing_1_temperature_std', 'Gearbox_bearing_2_temperature',
+       'Gearbox_bearing_2_temperature_min',
+       'Gearbox_bearing_2_temperature_max',
+       'Gearbox_bearing_2_temperature_std', 'Gearbox_inlet_temperature',
+       'Gearbox_inlet_temperature_min', 'Gearbox_inlet_temperature_max',
+       'Gearbox_inlet_temperature_std', 'Gearbox_oil_sump_temperature',
+       'Gearbox_oil_sump_temperature_min', 'Gearbox_oil_sump_temperature_max',
+       'Gearbox_oil_sump_temperature_std', 'Nacelle_angle',
+       'Nacelle_angle_min', 'Nacelle_angle_max', 'Nacelle_angle_std',
+       'Nacelle_temperature', 'Nacelle_temperature_min',
+       'Nacelle_temperature_max', 'Nacelle_temperature_std',
+       'Absolute_wind_direction', 'Outdoor_temperature',
+       'Outdoor_temperature_min', 'Outdoor_temperature_max',
+       'Outdoor_temperature_std', 'Grid_frequency', 'Grid_frequency_min',
+       'Grid_frequency_max', 'Grid_frequency_std', 'Grid_voltage',
+       'Grid_voltage_min', 'Grid_voltage_max', 'Grid_voltage_std',
+       'Rotor_speed', 'Rotor_speed_min', 'Rotor_speed_max', 'Rotor_speed_std',
+       'Rotor_bearing_temperature', 'Rotor_bearing_temperature_min',
+       'Rotor_bearing_temperature_max', 'Rotor_bearing_temperature_std'
+       ]
+
+other_features = ['Absolute_wind_direction_c', 'Nacelle_angle_c']
+
+comparison.preprocessing(
+    numerical_features,
+    other_features,
+    nknots=4,
+    poly_order=2,
+    scaler='standard',
+    )
+
+best_params_list, best_score_list, best_estimator_list, predict_score_list = comparison.run_comparison(
+                    preproc=['base'],
+                    model_param={
+                        'linear_regression': {},
+                        # 'ridge': {},
+                        'lasso': {},
+                        'elasticnet': {},
+                        'randomforest': {
+                            'regressor__n_estimators' : [100, 200, 300, 1000], # Nombre d'arbres dans la forêt. defaut 100
+                            'regressor__max_depth' : [None, 10, 20, 30], # Profondeur maximale des arbres. Si None, les arbres sont développés jusqu'à ce que toutes les feuilles soient pures ou que chaque feuille contienne moins que min_samples_split échantillons
+                            'regressor__max_features': [3, 'sqrt'], # Nombre maximum de caractéristiques considérées pour chaque split (division d'un nœud en deux sous-nœuds)
+                            },
+                        'grd_boosting': {
+                            'regressor__learning_rate' : [.00, .01, .1, 1],
+                            'regressor__max_depth' : [3, 7, 9],
+                            'regressor__subsample' : [0.5, 0.7, 1],
+                            'regressor__n_estimators' : [100, 200, 300, 1000]
+                            }
+                        },
+                    nfolds=5,
+                    verbose=True
+                    )
---
+++ regression_model_comparison.py
@@ -0,0 +1,254 @@
+# Compare Algorithms
+import pandas as pd
+import time
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LinearRegression, Ridge, ElasticNet, Lasso, LassoCV, ElasticNetCV
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder # Utile ?
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import PolynomialFeatures, SplineTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import make_scorer, mean_absolute_error, mean_squared_error
+
+import mlflow
+import mlflow.sklearn
+
+class RegressionModelComparison:
+    """
+    Class for regression model comparison using scikit-learn
+    """
+
+    def __init__(self, X, y, scorings=['mae'], test_size=0.1, seed=3):
+        self.SEED = seed # prepare configuration for cross validation test harness
+        self.X = X
+        self.y = y
+        self.test_size = test_size
+        self.scorings = scorings
+        self.allowed_models = ['linear_regression', 'ridge', 'lasso', 'elasticnet', 'randomforest', 'grd_boosting']
+        self.allowed_scalers = ['standard', 'minmax']
+        self.allowed_preproc = ['base', 'poly', 'splines']
+        self.allowed_scorings = ['mae', 'mse']
+
+        self.make_scorers(scorings)
+        self.split_dataset() # Splitting dataset
+
+    def split_dataset(self):
+        print(f"##### Splitting Dataset with test_size = {self.test_size} and random_state = {self.SEED}")
+        self.X_train_val, self.X_test, self.y_train_val, self.y_test = train_test_split(self.X, self.y, test_size=self.test_size, random_state=self.SEED)
+        # X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2, random_state=self.SEED)
+
+    def make_scorers(self, scorings):
+        self.scorers = {}
+        for score in scorings:
+            if score == 'mae':
+                self.scorers[score] = {}
+                self.scorers[score]['name'] = 'neg_mean_absolute_error'
+                self.scorers[score]['metric'] = make_scorer(mean_absolute_error)
+            elif score == 'mse':
+                self.scorers[score] = {}
+                self.scorers[score]['name'] = 'neg_mean_squared_error'
+                self.scorers[score]['metric'] = make_scorer(mean_squared_error)
+            else:
+                raise ValueError(f"Wrong metric name. Names are to be one of : {self.allowed_scorings}")
+
+    def preprocessing(self, numerical_features, other_features, nknots=4, poly_order=2, scaler='standard'):
+
+        #### Pipeline steps
+
+        # Tester différents préprocessings avec polynomes, splines, différents scalers if necessary (depends on the algorithm)
+
+        ## SCALER
+        if scaler == 'standard':
+            scaler = StandardScaler()
+        elif scaler == 'minmax':
+            scaler = MinMaxScaler()
+        else:
+            raise ValueError(f"Wrong model name. Names are to be one of : {self.allowed_scalers}")
+
+        ## COLUMNS TRANSFORMERS
+        numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy= 'mean')),
+            ('scaler', StandardScaler())
+        ])
+        # categorical_transformer = Pipeline(steps=[
+        #     ('imputer', SimpleImputer(strategy= 'most_frequent')),
+        #     ('ohe', OneHotEncoder(handle_unknown= 'ignore')) # (drop='if_binary', sparse_output=False)
+        # ])
+
+        poly_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer()),
+            ('poly', PolynomialFeatures(poly_order)), #interaction_only= True
+            ('scaler', StandardScaler())
+        ])
+
+        spline_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='mean')),  # Imputation des valeurs manquantes
+            ('spline', SplineTransformer(degree=3, n_knots=nknots, include_bias=False)),
+            ('scaler', StandardScaler())  # Mise à l'échelle des splines
+        ])
+
+        ## PROCESSORS
+        self.preprocessor = ColumnTransformer(transformers= [
+            ('num', numeric_transformer, numerical_features),
+            # ('cat', categorical_transformer, categorical_features)
+        ])
+
+        self.preprocessor_ps = ColumnTransformer(transformers=[
+            ('num', numeric_transformer, numerical_features),
+            # ('cat', categorical_transformer, categorical_features),
+            ('poly', poly_transformer, other_features),
+        ])
+
+        self.preprocessor_spline = ColumnTransformer(transformers=[
+            ('num', numeric_transformer, numerical_features),  # Caractéristiques numériques classiques
+            # ('cat', categorical_transformer, categorical_features),  # Caractéristiques catégorielles
+            # ('poly', poly_transformer, other_features),  # Caractéristiques pour PolynomialFeatures
+            ('spline', spline_transformer, other_features)  # Caractéristiques pour SplineTransformer
+        ])
+
+        print(f"##### Preprocessors prepared : ")
+        print(f"## 'base' :  {self.preprocessor}")
+        print(f"## 'poly' : {self.preprocessor_ps}")
+        print(f"## 'splines' : {self.preprocessor_spline}")
+
+
+    def run_comparison(
+            self,
+            preproc=['base'],
+            model_param={'linear_regression': {}},
+            nfolds=5,
+            verbose=False
+            ):
+        """
+        Tester les différents modèles
+        Pour chaque modèle + chaque preprocessing tester différentes valeurs d'hyperparamètres en grid-search
+        """
+
+        preprocs = []
+        for prep in preproc:
+            if prep == 'base':
+                preprocs.append(self.preprocessor)
+            elif prep == 'poly':
+                preprocs.append(self.preprocessor_ps)
+            elif prep == 'splines':
+                preprocs.append(self.preprocessor_spline)
+            else:
+                raise ValueError(f"Wrong preprocessor name. Names are to be one of : {self.allowed_preproc}")
+
+        models = []
+        for mdl, params in model_param.items():
+            if mdl == 'linear_regression':
+                models.append((mdl, LinearRegression(), params))
+            elif mdl == 'ridge':
+                models.append((mdl, Ridge(), params))
+            elif mdl == 'lasso':
+                models.append((mdl, LassoCV(), params))
+            elif mdl == 'elasticnet':
+                models.append((mdl, ElasticNetCV(), params))
+            elif mdl == 'randomforest':
+                models.append((mdl, RandomForestRegressor(), params))
+            elif mdl == 'grd_boosting':
+                models.append((mdl, GradientBoostingRegressor(), params))
+        #   elif model == 'smf':
+        #        pass
+        #   elif mdl == 'xgboost':
+        #        pass
+            else:
+                raise ValueError(f"Wrong model name. Names are to be one of : {self.allowed_models}")
+
+        # Tout ajouter à une table pour tout sauvegarder (MLFLOW !)
+
+        best_params_list = []
+        best_score_list = []
+        best_estimator_list =[]
+        predict_score_list = []
+
+        print("###### Start comparison ######")
+
+        mlflow.set_tracking_uri('http://localhost:5000')
+        with mlflow.start_run():
+            start_time = time.time()
+
+            for preprocessor in preprocs :
+                print(f"Using preprocessor : {preprocessor}")
+
+                for reg_name, regressor, params in models :
+                    print(f"Using regressor : {regressor}")
+                    if verbose:
+                        print(f"with specific params {params}")
+
+                    # Pipeline
+                    pip = Pipeline(steps=[
+                        ('preproc', preprocessor),
+                        ('regressor', regressor),
+                    ])
+
+                    if reg_name in ['lasso', 'elasticnet']:
+                        grid = regressor.fit(self.X_train_val, self.y_train_val)
+                        score_train_val = self.scorers['mae']['metric'](estimator=grid, X=self.X_train_val, y_true=self.y_train_val)
+                        prevision = self.scorers['mae']['metric'](estimator=grid, X=self.X_test, y_true=self.y_test)                
+                    
+                        if verbose:
+                            print(grid)
+                            print(score_train_val) # MAE
+                            print(grid.get_params())                        
+
+                        best_params = grid.get_params() # Model is fitted on all training set at the end of CV with best params
+                        best_params_list.append(best_params)
+                        best_score_list.append(score_train_val)
+                        best_estimator_list.append(grid)
+                        predict_score_list.append(prevision)
+                            
+                    else:
+                        # Ce RIDGE ne fonctionne PAS
+                        # if reg_name == 'ridge': # Using better coefficients for Ridge (from class with Eric)
+                        #     path_ridge = LassoCV().fit(self.X_train_val, self.y_train_val).alphas_ * 100
+                        #     params = {"ridge__alpha": path_ridge}
+
+                        # Grid Search
+                        grid = GridSearchCV(
+                                estimator=pip, 
+                                param_grid=params, 
+                                cv=nfolds, 
+                                scoring=[value['name'] for key, value in self.scorers.items()], 
+                                refit=self.scorers['mae']['name']
+                            ).fit(self.X_train_val, self.y_train_val)
+                    
+                        if verbose:
+                            print(grid.best_estimator_)
+                            print(grid.best_score_)
+                            print(grid.best_params_)
+
+                        best_params = grid.best_params_
+                        best_params_list.append(best_params)
+                        best_score_list.append(grid.best_score_)
+                        best_estimator_list.append(grid.best_estimator_)
+                        prevision = grid.score(self.X_test, self.y_test)
+                        predict_score_list.append(prevision)
+                    with mlflow.start_run(nested=True):
+                        # Consigner les paramètres et les métriques
+                        for param, value in best_params.items():
+                            mlflow.log_param(param, value)
+                        mlflow.log_metric(self.scorings[0], prevision)
+                        # Consigner le modèle
+                        mlflow.sklearn.log_model(regressor, "model")
+
+            
+            duration = time.time() - start_time
+            mlflow.log_param("comparison_duration", duration)
+
+        return best_params_list, best_score_list, best_estimator_list, predict_score_list
+
+
+# Tester l'implémentation sur un tout petit JDD ! Notamment pour MLFLOW
+
+# Encapsuler la classe dans mlflow
+
+
+    # if np.isnan(transformed_data).any():
+    #     raise ValueError("Il y a des valeurs manquantes (NaN) dans les données transformées.")
+    # else:
+    #     print("Aucune valeur manquante (NaN) dans les données transformées.")
